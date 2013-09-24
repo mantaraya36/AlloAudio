c207448114fb4512e4a8643b79512d586e7e77f7
Andres Cabrera <andres@lenovo-W530>
2013-09-24 11:41:35 -0700

    Minor cleanup

--- osc_control.c
+++ osc_control.c
@@ -11,7 +11,7 @@ struct oscdata {
     connection_data_t *pp;
 };
 
-/* OSC functions */
+/* OSC handler functions */
 void osc_error(int num, const char *msg, const char *path)
 {
     printf("liblo server error %d in path %s: %s\n", num, path, msg);
@@ -22,9 +22,7 @@ int global_gain_handler(const char *path, const char *types, lo_arg ** argv,
                 int argc, void *data, void *user_data)
 {
     connection_data_t *pp = (connection_data_t *) user_data;
-    /* example showing pulling the argument values out of the argv array */
-    printf("%s <- %f\n", path, argv[0]->f);
-    fflush(stdout);
+    printf("%s <- %f\n", path, argv[0]->f); fflush(stdout);
 
     set_global_gain(pp, argv[0]->f);
     return 0;
@@ -34,9 +32,7 @@ int gain_handler(const char *path, const char *types, lo_arg ** argv,
                 int argc, void *data, void *user_data)
 {
     connection_data_t *pp = (connection_data_t *) user_data;
-    /* example showing pulling the argument values out of the argv array */
-    printf("%s <- f:%f\n", path, argv[0]->f);
-    fflush(stdout);
+    printf("%s <- f:%f\n", path, argv[0]->f); fflush(stdout);
 
     set_gain(pp, argv[0]->i, argv[0]->f);
     return 0;
@@ -46,9 +42,7 @@ int mute_handler(const char *path, const char *types, lo_arg ** argv,
                 int argc, void *data, void *user_data)
 {
     connection_data_t *pp = (connection_data_t *) user_data;
-    /* example showing pulling the argument values out of the argv array */
-    printf("%s <- f:%i\n", path, argv[0]->i);
-    fflush(stdout);
+    printf("%s <- f:%i\n", path, argv[0]->i); fflush(stdout);
 
     set_mute_all(pp, argv[0]->i);
     return 0;
@@ -58,9 +52,7 @@ int clipper_handler(const char *path, const char *types, lo_arg ** argv,
                 int argc, void *data, void *user_data)
 {
     connection_data_t *pp = (connection_data_t *) user_data;
-    /* example showing pulling the argument values out of the argv array */
-    printf("%s <- f:%i\n", path, argv[0]->i);
-    fflush(stdout);
+    printf("%s <- f:%i\n", path, argv[0]->i); fflush(stdout);
 
     set_clipper_on(pp, argv[0]->i);
     return 0;
@@ -91,14 +83,10 @@ oscdata_t *create_osc(const char *port, connection_data_t *pp)
     oscdata_t *od = (oscdata_t *) malloc(sizeof(oscdata_t));
     od->st = lo_server_thread_new(port, osc_error);
 
-
-    /* add method that will match the path /foo/bar, with two numbers, coerced
-     * to float and int */
     lo_server_thread_add_method(od->st, "/Alloaudio/global_gain", "f", global_gain_handler, pp);
     lo_server_thread_add_method(od->st, "/Alloaudio/gain", "if", gain_handler, pp);
     lo_server_thread_add_method(od->st, "/Alloaudio/mute_all", "i", mute_handler, pp);
     lo_server_thread_add_method(od->st, "/Alloaudio/clipper_on", "i", mute_handler, pp);
-
 
     /* add method that will match any path and args */
     lo_server_thread_add_method(od->st, NULL, NULL, generic_handler, NULL);
